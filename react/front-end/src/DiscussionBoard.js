0229cac0dd935518dccb706aa97f9df9f93cea88
Zach Zloof <zachzloof@hotmail.com>
2022-05-10 15:35:31 +0100

    QC-34 #comment ZZ: Additional questions added to the discussion board

--- react/front-end/src/DiscussionBoard.js
+++ react/front-end/src/DiscussionBoard.js
@@ -11,37 +11,79 @@ class DiscussionBoard extends Component {
     constructor() {
         super();
         this.state = {
-            records:[]
+            records: []
         };
     }
+    componentDidMount(){ //usual equivalent useEffect(getComments, [])
+        this.getComments();
+    }
 
-    getComments() {
-        let refPromise = axios.get("http://localhost:4005/getComments");
-        refPromise.then((response) => {
-            console.log(response.data)
-            this.setState({records:response.data})
-        });
+    async getComments() {
+        let response = await axios.get("http://localhost:4005/getComments");
+        console.log(response.data)
+        this.setState({ records: response.data })
     }
 
     render() {
         console.log(this.state.records)
         return (<div>
             <h1>QA CINEMA DISCUSSION BOARD!</h1>
-            <button onClick={() => this.getComments()}>REFRESH </button>
             <div>
-            <h3>What is your favourite movie?</h3>
-            <table>
-            {this.state.records.map((data) =>
-                        
-                        <tr>
+                <h3>1.What is your favourite movie?</h3>
+                <table>
+                    {this.state.records.map((data) =>
+                        (data.post == 1) &&
+                        (<tr>
                             <td><b> {data.user} </b> </td>
                             <td> {data.likes} </td>
                             <td> {data.dislikes} </td>
                             <td> {data.body} </td>
-                        </tr>
+                        </tr>)
                     )
                     }
-                    </table>
+                </table>
+
+                <h3>2.What was your best cinema experience?</h3>
+                <table>
+                    {this.state.records.map((data) =>
+                        (data.post == 2) &&
+                        (<tr>
+                            <td><b> {data.user} </b> </td>
+                            <td> {data.likes} </td>
+                            <td> {data.dislikes} </td>
+                            <td> {data.body} </td>
+                        </tr>)
+                    )
+                    }
+                </table>
+
+                <h3>3. Which film are you most looking forward too?</h3>
+                <table>
+                    {this.state.records.map((data) =>
+                        (data.post == 3) &&
+                        (<tr>
+                            <td><b> {data.user} </b> </td>
+                            <td> {data.likes} </td>
+                            <td> {data.dislikes} </td>
+                            <td> {data.body} </td>
+                        </tr>)
+                    )
+                    }
+                </table>
+
+                <h3>4. MARVEL OR DC??</h3>
+                <table>
+                    {this.state.records.map((data) =>
+                        (data.post == 4) &&
+                        (<tr>
+                            <td><b> {data.user} </b> </td>
+                            <td> {data.likes} </td>
+                            <td> {data.dislikes} </td>
+                            <td> {data.body} </td>
+                        </tr>)
+                    )
+                    }
+                </table>
             </div>
         </div>)
 
