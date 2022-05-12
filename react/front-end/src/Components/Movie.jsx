e04b36dd8b96797162b6cb78545d54e392680900
jdbonapos1 <johnbonapos@outlook.com>
2022-05-12 17:17:18 +0100

    QC-12 #comment JD; added movie listings using an API

--- react/front-end/src/Components/Movie.jsx
+++ react/front-end/src/Components/Movie.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+
+
 // movie listing page using jsx
 const Movie = () => 
 { 
 
-    // FROM HERE
+    // Const for API Data
     const[avengersData, setAvengersData] =useState([]);
     //
     const[narutoData, setNarutoData] =useState([]);
@@ -16,13 +18,84 @@ const Movie = () =>
     const[sonicData, setSonicData] =useState([]);
     //
     const[morbiusData, setMorbiusData] =useState([]);
+    //
+    const[topgunData, setTopgunData] =useState([]);
+    //
+    const[drstrangeData, setDrstrangeData] =useState([]);
+    //
+    const[fantasticbeastsData, setFantasticbeastsData] =useState([]);
+    //
+    const[spidermanData, setSpidermanData] =useState([]);
+    //
+    const[thelostcityData, setThelostcityData] =useState([]);
 
     
 
 
     const[loaded, setLoaded] =useState(false);
-    // HERE 
+    
     useEffect(()=>{
+
+        axios.get("http://www.omdbapi.com/?s=The lost city&apikey=2c98117c")
+        .then((res)=>{
+            console.log(res)
+            setSpidermanData(res.data) 
+
+           
+
+        }).catch((err)=>{ console.log(err)
+            setLoaded(true)
+            
+        }
+        )
+        axios.get("http://www.omdbapi.com/?i=tt10872600&apikey=2c98117c")
+        .then((res)=>{
+            console.log(res)
+            setSpidermanData(res.data) 
+
+           
+
+        }).catch((err)=>{ console.log(err)
+            setLoaded(true)
+            
+        }
+        )
+        axios.get("http://www.omdbapi.com/?i=tt4123432&apikey=2c98117c")
+        .then((res)=>{
+            console.log(res)
+            setFantasticbeastsData(res.data) 
+
+           
+
+        }).catch((err)=>{ console.log(err)
+            setLoaded(true)
+            
+        }
+        )
+        axios.get("http://www.omdbapi.com/?i=tt9419884&apikey=2c98117c")
+        .then((res)=>{
+            console.log(res)
+            setDrstrangeData(res.data) 
+
+           
+
+        }).catch((err)=>{ console.log(err)
+            setLoaded(true)
+            
+        }
+        )
+        axios.get("http://www.omdbapi.com/?i=tt1745960&apikey=2c98117c")
+        .then((res)=>{
+            console.log(res)
+            setTopgunData(res.data) 
+
+           
+
+        }).catch((err)=>{ console.log(err)
+            setLoaded(true)
+            
+        }
+        )
         axios.get("http://www.omdbapi.com/?i=tt5108870&apikey=2c98117c")
         .then((res)=>{
             console.log(res)
@@ -103,40 +176,96 @@ const Movie = () =>
 
     // HTML section
 
+    // todo: link images to a trailer
+
 
     if(loaded){
         return (
+
         <>
-         <img src={avengersData.Poster} alt="avengers" />
+
+        <h1> QA CINEMAS </h1>
+
+
+       
+         <img src={avengersData.Poster} alt="avengers" id="avengers" />
          <h1> {avengersData.Title}</h1>
-         <h2 id="movie-title"> A QA Cinemas special showing especially for marvel fans who want to rewatch the action in the big Screen! </h2>
-         <h4> {avengersData.Plot}</h4>
-
-         <img src={narutoData.Poster} alt="naruto" />
-         <h1> {narutoData.Title}</h1>
-         <h2 id="movie-title"> A QA Cinemas special for all shinobi to enjoy in glorious IMAX</h2>
-         <h4> {narutoData.Plot}</h4>
-
-         <img src={sonicData.Poster} alt="sonic" />
-         <h1> {sonicData.Title}</h1>
-         <h2 id="movie-title"> Faster than the flash?!</h2>
-         <h4>  {sonicData.Plot} </h4>
-
-         <img src={batmanData.Poster} alt="batman" />
+         <h4> <b>Directors:</b> {avengersData.Director} </h4>
+         <h4> <b>Cast:</b> {avengersData.Actors}</h4> 
+         <h4> <b>Plot:</b> {avengersData.Plot}</h4>
+         <h4> <b>More Info...</b></h4>
+         
+         
+         <img src={batmanData.Poster} alt="batman" id="batman" />
          <h1> {batmanData.Title}</h1>
-         <h2 id="movie-title"> The Batman Returns in 2022 </h2>
-         <h4> {batmanData.Plot}</h4>
+         <h4> <b>Directors:</b> {batmanData.Director} </h4>
+         <h4> <b>Cast:</b> {batmanData.Actors}</h4>
+         <h4> <b>Synopsis:</b> {batmanData.Plot}</h4>
+         <h4> <b>More Info...</b></h4>
 
-         <img src={spongebobData.Poster} alt="spongebob" />
-         <h1> {spongebobData.Title}</h1>
-         <h2 id="movie-title"> The best film of all time?</h2>
-         <h4> {spongebobData.Plot} </h4>
+         <img src={drstrangeData.Poster} alt="drstrange" id="drstrange" />
+         <h1> {drstrangeData.Title}</h1>
+         <h4> <b>Directors:</b> {drstrangeData.Director} </h4>
+         <h4> <b>Cast:</b> {drstrangeData.Actors}</h4>
+         <h4> <b>Synopsis:</b> {drstrangeData.Plot}</h4>
+         <h4> <b>More Info...</b></h4>
 
+         
+         <img src={fantasticbeastsData.Poster} alt="fantasticbeasts" id="fantasticbeasts" />
+         <h1> {fantasticbeastsData.Title}</h1>
+         <h4> <b>Directors:</b> {fantasticbeastsData.Director} </h4>
+         <h4> <b>Cast</b>: {fantasticbeastsData.Actors}</h4>
+         <h4> <b>Synopsis:</b> {fantasticbeastsData.Plot}</h4>
+         <h4> <b>More Info...</b></h4>
 
-         <img src={morbiusData.Poster} alt="morbius" />
+         
+         <img src={morbiusData.Poster} alt="morbius" id="morbius" />
          <h1> {morbiusData.Title}</h1>
-         <h2 id="movie-title"> A pretty shit film...</h2>
-         <h4> {morbiusData.Plot} </h4>
+         <h4> <b>Directors:</b> {morbiusData.Director} </h4>
+         <h4> <b>Cast:</b> {morbiusData.Actors} </h4>
+         <h4> <b>Synopsis:</b> {morbiusData.Plot} </h4>
+         <h4> <b>More Info...</b></h4>
+
+
+         <img src={narutoData.Poster} alt="naruto" id="naruto" />
+         <h1> {narutoData.Title}</h1>
+         <h4> <b>Directors:</b> {narutoData.Director} </h4>
+         <h4> <b>Cast:</b> {narutoData.Actors}</h4>
+         <h4> <b>Synopsis:</b> {narutoData.Actors}</h4>
+         <h4> <b>More Info...</b></h4>
+
+         <img src={sonicData.Poster} alt="sonic" id="sonic" />
+         <h1> {sonicData.Title}</h1>
+         <h4> <b>Directors:</b> {sonicData.Director} </h4>
+         <h4> <b>Cast:</b> {sonicData.Actors} </h4>
+         <h4> <b>Synopsis:</b>  {sonicData.Plot} </h4>
+         <h4> <b>More Info...</b></h4>
+
+         <img src={spidermanData.Poster} alt="spiderman" id="spiderman" />
+         <h1> {spidermanData.Title}</h1>
+         <h4> <b>Directors:</b> {spidermanData.Director}</h4>
+         <h4> <b>Cast:</b> {spidermanData.Actors}</h4>
+         <h4> <b>Synopsis:</b> {spidermanData.Plot}</h4>
+         <h4> <b>More Info...</b></h4>
+
+         
+         <img src={spongebobData.Poster} alt="spongebob" id="spongebob" />
+         <h1> {spongebobData.Title}</h1>
+         <h4> <b>Directors:</b> {spongebobData.Director} </h4>
+         <h4> <b>Cast:</b> {spongebobData.Actors} </h4>
+         <h4> <b>Synopsis:</b> {spongebobData.Plot} </h4>
+         <h4> <b>More Info...</b></h4>
+
+
+         <img src={topgunData.Poster} alt="topgun" id="topgun" />
+         <h1> {topgunData.Title}</h1>
+         <h4> <b>Directors:</b> {topgunData.Director} </h4>
+         <h4> <b>Cast:</b> {topgunData.Actors} </h4>
+         <h4> <b>Synopsis:</b> {topgunData.Plot}</h4>
+         <h4> <b>MoreInfo...</b></h4>
+         
+
+
           </>
         
     )
@@ -145,6 +274,7 @@ const Movie = () =>
     else{return(
         <h1> loading... </h1>
     )}
+    
     
 
 
