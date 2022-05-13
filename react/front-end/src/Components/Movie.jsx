767902a3ac985b0cbd999578ca59ff2d391b4d56
jdbonapos1 <johnbonapos@outlook.com>
2022-05-13 12:32:44 +0100

    QC-12 #comment JD: Added supporting text for movie posters incuding title,actor,director,genre and runtime

--- react/front-end/src/Components/Movie.jsx
+++ react/front-end/src/Components/Movie.jsx
@@ -156,22 +156,7 @@ const Movie = () =>
             
         }
         )
-        axios.get("http://www.omdbapi.com/?i=tt6342474&apikey=2c98117c")
-        .then((res)=>{
-            
-            setNarutoData(res.data) 
-            setLoaded(true)
-
-           
-
-        }).catch((err)=>{ console.log(err)
-            setLoaded(true)
-            
-        }
-        )
        
-
-
     },[]) 
 
     // HTML section
@@ -191,8 +176,8 @@ const Movie = () =>
          <h1> {avengersData.Title}</h1>
          <h4> <b>Directors:</b> {avengersData.Director} </h4>
          <h4> <b>Cast:</b> {avengersData.Actors}</h4> 
-         <h4> <b>Plot:</b> {avengersData.Plot}</h4>
-         <h4> <b>Run time: {avengersData.Runtime}</b></h4>
+         <h4> <b>Synopsis:</b> {avengersData.Plot}</h4>
+         <h4> <b>Run time:</b> {avengersData.Runtime}</h4>
          <h4> <b> Genre: </b> {avengersData.Genre}</h4>
          
          <a href="https://youtu.be/mqqft2x_Aa4" target="_blank"> 
@@ -201,7 +186,9 @@ const Movie = () =>
          <h4> <b>Directors:</b> {batmanData.Director} </h4>
          <h4> <b>Cast:</b> {batmanData.Actors}</h4>
          <h4> <b>Synopsis:</b> {batmanData.Plot}</h4>
-         <h4> <b>More Info...</b></h4>
+         <h4> <b>Run time: </b>{batmanData.Runtime}</h4>
+         <h4> <b> Genre: </b> {batmanData.Genre}</h4>
+         
          
 
          <a href="https://youtu.be/aWzlQ2N6qqg" target="_blank">
@@ -210,7 +197,9 @@ const Movie = () =>
          <h4> <b>Directors:</b> {drstrangeData.Director} </h4>
          <h4> <b>Cast:</b> {drstrangeData.Actors}</h4>
          <h4> <b>Synopsis:</b> {drstrangeData.Plot}</h4>
-         <h4> <b>More Info...</b></h4>
+         <h4> <b>Run time:</b> {drstrangeData.Runtime}</h4>
+         <h4> <b> Genre: </b> {drstrangeData.Genre}</h4>
+         
 
          <a href="https://youtu.be/Y9dr2zw-TXQ" target="_blank"> 
          <img src={fantasticbeastsData.Poster} alt="fantasticbeasts" id="fantasticbeasts" /> </a>
@@ -218,7 +207,8 @@ const Movie = () =>
          <h4> <b>Directors:</b> {fantasticbeastsData.Director} </h4>
          <h4> <b>Cast</b>: {fantasticbeastsData.Actors}</h4>
          <h4> <b>Synopsis:</b> {fantasticbeastsData.Plot}</h4>
-         <h4> <b>More Info...</b></h4>
+         <h4> <b>Run time: </b>{fantasticbeastsData.Runtime}</h4>
+         <h4> <b> Genre: </b> {fantasticbeastsData.Genre}</h4>
 
          <a href="https://youtu.be/oZ6iiRrz1SY" target="_blank">
          <img src={morbiusData.Poster} alt="morbius" id="morbius" /> </a>
@@ -226,15 +216,9 @@ const Movie = () =>
          <h4> <b>Directors:</b> {morbiusData.Director} </h4>
          <h4> <b>Cast:</b> {morbiusData.Actors} </h4>
          <h4> <b>Synopsis:</b> {morbiusData.Plot} </h4>
-         <h4> <b>More Info...</b></h4>
-
-         <a href="https://youtu.be/Qyonn5Vbg7s" target="_blank"> 
-         <img src={narutoData.Poster} alt="naruto" id="naruto" /> </a>
-         <h1> {narutoData.Title}</h1>
-         <h4> <b>Directors:</b> {narutoData.Director} </h4>
-         <h4> <b>Cast:</b> {narutoData.Actors}</h4>
-         <h4> <b>Synopsis:</b> {narutoData.Actors}</h4>
-         <h4> <b>More Info...</b></h4>
+         <h4> <b>Run time: </b>{morbiusData.Runtime}</h4>
+         <h4> <b> Genre: </b> {morbiusData.Genre}</h4>
+     
 
          <a href="https://youtu.be/G5kzUpWAusI" target="_blank"> 
          <img src={sonicData.Poster} alt="sonic" id="sonic" /> </a>
@@ -242,7 +226,8 @@ const Movie = () =>
          <h4> <b>Directors:</b> {sonicData.Director} </h4>
          <h4> <b>Cast:</b> {sonicData.Actors} </h4>
          <h4> <b>Synopsis:</b>  {sonicData.Plot} </h4>
-         <h4> <b>More Info...</b></h4>
+         <h4> <b>Run time:</b> {sonicData.Runtime}</h4>
+         <h4> <b> Genre: </b> {sonicData.Genre}</h4>
 
          <a href="https://youtu.be/JfVOs4VSpmA" target="_blank"> 
          <img src={spidermanData.Poster} alt="spiderman" id="spiderman" /> </a>
@@ -250,8 +235,8 @@ const Movie = () =>
          <h4> <b>Directors:</b> {spidermanData.Director}</h4>
          <h4> <b>Cast:</b> {spidermanData.Actors}</h4>
          <h4> <b>Synopsis:</b> {spidermanData.Plot}</h4>
-         <h4> <b>More Info...</b></h4>
-
+         <h4> <b>Run time:</b> {spidermanData.Runtime}</h4>
+         <h4> <b> Genre: </b> {spidermanData.Genre}</h4>
          
          <a href="https://youtu.be/0ec9Thn50hs" target="_blank">
          <img src={spongebobData.Poster} alt="spongebob" id="spongebob" /> </a>
@@ -259,7 +244,8 @@ const Movie = () =>
          <h4> <b>Directors:</b> {spongebobData.Director} </h4>
          <h4> <b>Cast:</b> {spongebobData.Actors} </h4>
          <h4> <b>Synopsis:</b> {spongebobData.Plot} </h4>
-         <h4> <b>More Info...</b></h4>
+         <h4> <b>Run time:</b> {spongebobData.Runtime}</h4>
+         <h4> <b> Genre: </b> {spongebobData.Genre}</h4>
 
          <a href="https://youtu.be/nfKO9rYDmE8" target="_blank">
          <img src={thelostcityData.Poster} alt="topgun" id="topgun" /> </a>
@@ -267,17 +253,19 @@ const Movie = () =>
          <h4> <b>Directors:</b> {thelostcityData.Director} </h4>
          <h4> <b>Cast:</b> {thelostcityData.Actors} </h4>
          <h4> <b>Synopsis:</b> {thelostcityData.Plot}</h4>
-         <h4> <b>MoreInfo...</b></h4>
+         <h4> <b>Run time:</b> {thelostcityData.Runtime}</h4>
+         <h4> <b> Genre: </b> {thelostcityData.Genre}</h4>
+       
          
          <a href="https://youtu.be/giXco2jaZ_4" target="_blank"> 
          <img src={topgunData.Poster} alt="topgun" id="topgun" /> </a>
          <h1> {topgunData.Title}</h1>
          <h4> <b>Directors:</b> {topgunData.Director} </h4>
          <h4> <b>Cast:</b> {topgunData.Actors} </h4>
-         <h4> <b>Synopsis:</b> {topgunData.Plot}</h4>
-         <h4> <b>MoreInfo...</b></h4>
-         
-
+         <h4> <b>Synopsis: </b> {topgunData.Plot}</h4>
+         <h4> <b>Run time: </b> {topgunData.Runtime}</h4>
+         <h4> <b> Genre: </b> {topgunData.Genre}</h4>
+        
 
           </>
         
