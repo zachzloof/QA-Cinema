7759320460c3e65300b34d4c4f4309ddc07fcfca
owencornock <owen52new@gmail.com>
2022-05-19 11:25:39 +0100

    CSS fixes

--- react/front-end/src/ToDo/toDo.js
+++ react/front-end/src/ToDo/toDo.js
@@ -6,40 +6,40 @@ export default function ToDo() {
     return(
         <div>
         <h1>Things to do!</h1>
-        <div className="box">
+        <div className="boxTwo">
         
-        <img className="image" src="https://www.planetware.com/photos-large/ENG/england-london-tower-of-london-from-water.jpg"></img>
-        <div className="text">
+        <img className="imageTwo" src="https://www.planetware.com/photos-large/ENG/england-london-tower-of-london-from-water.jpg"></img>
+        <div className="textTwo">
         <h1>Tower Of London</h1>
         <p>The Tower of London, officially Her Majesty's Royal Palace and Fortress of the Tower of London, is a historic castle on the north bank of the River Thames in central London.</p>
         <p>Current offer: <strong>Two for one cinema tickets when purchased with tour tickets</strong></p>
         <p>Contacts us on phone:01665365847</p>
     </div>
         </div>
-        <div className="box">
+        <div className="boxTwo">
         
-        <img className="image" src="https://lh5.googleusercontent.com/p/AF1QipPSZr4DRxBYpNaQVoGMB9mQ5nMWi5d2AWO_8m_Q=w408-h612-k-no"></img>
-        <div className="text">
+        <img className="imageTwo" src="https://lh5.googleusercontent.com/p/AF1QipPSZr4DRxBYpNaQVoGMB9mQ5nMWi5d2AWO_8m_Q=w408-h612-k-no"></img>
+        <div className="textTwo">
             <h1>Jack the Ripper Museum</h1>
         <p>Grisly museum on 6 floors in a Victorian house recounting stories of the 1888 East End murderer.</p>
         <p>Current offer: <strong>Free ticekts with any purchase of a family ticket at the Museum</strong></p>
         <p>Contacts us on phone:01656554812</p>
         </div>
     </div>
-        <div className="box">
+        <div className="boxTwo">
         
-        <img className="image" src="https://lh5.googleusercontent.com/p/AF1QipPCec1NNcQtlbWkYiIQllnE_w5_CVxaypRy8QgR=w408-h306-k-no"></img>
-        <div className="text">
+        <img className="imageTwo" src="https://lh5.googleusercontent.com/p/AF1QipPCec1NNcQtlbWkYiIQllnE_w5_CVxaypRy8QgR=w408-h306-k-no"></img>
+        <div className="textTwo">
             <h1>St Katharine Docks Marina</h1>
         <p>Fun for all the family. Make sure to visit this place to tire the kids out and you can have a relaxing time in the shops and restaurants. Just a short walk from the cinema, you won't regret it!</p>
         <p>Current offer: <strong>Kids tickets for free with any purchase in the cafes.</strong></p>
         <p>Contacts us on phone:225488965352</p>
         </div>
     </div>
-        <div className="box">
+        <div className="boxTwo">
     
-        <img className="image" src="https://lh5.googleusercontent.com/p/AF1QipNUcO-bLsJ7c4AHRfkq3sJtGs3SJ8DGGCY1ARlT=w408-h271-k-no"></img>
-        <div className="text">
+        <img className="imageTwo" src="https://lh5.googleusercontent.com/p/AF1QipNUcO-bLsJ7c4AHRfkq3sJtGs3SJ8DGGCY1ARlT=w408-h271-k-no"></img>
+        <div className="textTwo">
             <h1>BrewDog Outpost Tower Hill</h1>
         <p>For a more adult experience there are plenty of pubs close to the cinema. BrewDog offers craft beer and ales at great prices with enticing interiors.</p>
         <p>Current offer: <strong>Think you can beat the barman? Drink 4 litres of water and you get a weeks worth of unlimited tickets just for you</strong></p>
--- react/front-end/src/Upcomming/upcomming.js
+++ react/front-end/src/Upcomming/upcomming.js
@@ -7,72 +7,72 @@ export default function ControlledCarousel() {
 
     <div className='total'>
       <h1>Upcomming Films</h1>
-      <div className='box'>
+      <div className='boxOne'>
         <a href='https://www.youtube.com/watch?v=giXco2jaZ_4'>
         <img
-          className="image"
+          className="imageOne"
           src="https://www.vintagemovieposters.co.uk/wp-content/uploads/2020/10/IMG_5226-482x713.jpeg"
           alt="First slide"
         />
         </a>
-        <div className='text'>
+        <div className='textOne'>
         <h3>Top Gun: Maverick</h3>
         <p>Actors: Tom Cruise, Val Kilmer, Miles Teller</p>
         <p>Director: Joseph Kosinski</p>
         </div>
       </div>
-      <div className='box'>
+      <div className='boxOne'>
         <a href='https://www.youtube.com/watch?v=Ssh0Iez-4qY'>
         <img
-          className="image"
+          className="imageOne"
           src="https://m.media-amazon.com/images/M/MV5BZmQ1NDZjMTktMjFhZC00ZGY5LWEyMTMtNDhkOWM4NmMyZjI0XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg"
           alt="Second slide"
         />
 </a>
-        <div className='text'>
+        <div className='textOne'>
         <h3>Jurassic World Dominion</h3>
         <p>Actors: Sam Neill, Laura Dern, Jeff Goldblum, Chris Pratt</p>
         <p>Director: Colin Trevorrow</p>
 
       </div>
       </div>
-      <div className='box'>
+      <div className='boxOne'>
       <a href='https://www.youtube.com/watch?v=BwZs3H_UN3k'>
       <img
-        className="image"
+        className="imageOne"
         src="https://preview.redd.it/1444q03v0ox81.jpg?width=640&crop=smart&auto=webp&s=bddd4bc7b9121a411892ba7d6f57923deb9ed2eb"
         alt="Third slide"
       />
 </a>
-<div className='text'>
+<div className='textOne'>
       <h3>Lightyear</h3>
       <p>Actors: Chris Evans, Taikia Waititi, James Brolin</p>
       <p>Director: Angus MacLane</p>
     </div>
     </div>
-    <div className='box'>
+    <div className='boxOne'>
     <a href='https://www.youtube.com/watch?v=tgB1wUcmbbw'>
       <img
-        className="image"
+        className="imageOne"
         src="https://pbs.twimg.com/media/FQzsdTBVkAERLwI?format=jpg&name=medium"
         alt="Third slide"
       />
 </a>
-<div className='text'>
+<div className='textOne'>
       <h3>Thor: Love and Thunder</h3>
       <p>Actors: Chris Hemsworth, Christian Bale, Natali Portman</p>
       <p>Director: Taikia Waititi</p>
     </div>
     </div>
-    <div className='box'>
+    <div className='boxOne'>
     <a href='https://www.youtube.com/watch?v=MVpcszlYT7s'>
 <img
-  className="image"
+  className="imageOne"
   src="https://m.media-amazon.com/images/M/MV5BMDg5ZjNhNzYtM2RjNy00YTg4LTk2YWItODFmZGEzM2U5NzYzXkEyXkFqcGdeQXVyMTM1NjgyNDI@._V1_.jpg"
   alt="Third slide"
 />
 </a>
-<div className='text'>
+<div className='textOne'>
 <h3>The Devil's Light</h3>
 <p>Actors: Christian Navarro, Jacqueline Byers, Nicholas Ralph</p>
 <p>Director: Daniel Stamm</p>
